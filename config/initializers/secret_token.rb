c12f6efdeedc7113ba438e73d56c4cc5396be212
Patricio Bruna <pbruna@gmail.com>
2012-05-27 22:06:34 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Computometro::Application.config.secret_token = 'aad6eec3a42ed24c4285286d5ee726185e037780805f32108962ff0ca6720ea2e6e6dad6c47ed09ed0fcf3d6bb562eafcbb6971b7117ab5ddcf4e64f5a1e03ad'
